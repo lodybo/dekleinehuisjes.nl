605d47891d249649d478383f838a9e20c8759a90
Lody Borgers <l.borgers@taf.nl>
2023-02-26 21:43:38 +0100

    Wrote a first version of the schema and seeded database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,83 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import { PrismaClient } from '@prisma/client';
+import type { Recipe } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const arantja = await createUser('arantja');
+  const kaylee = await createUser('kaylee');
+  const tilai = await createUser('tilai');
+  const levy = await createUser('levy');
 
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
+  // create 2 recipes for each user
+  const recipes: Omit<Recipe, 'id' | 'createdAt' | 'updatedAt'>[] = [
+    {
+      title: 'Pasta with tomato sauce',
+      description: 'This is a delicious pasta with tomato sauce',
+      ingredients: ['Pasta', 'tomato sauce'],
+      steps: ['Boil pasta', 'mix with tomato sauce'],
+      persons: 2,
+      time: 30,
+      categories: ['Italian', 'Pasta'],
+      chefId: arantja.id,
+    },
+    {
+      title: 'Fried rice with egg',
+      description: 'This is a delicious fried rice with egg',
+      ingredients: ['Rice', 'egg'],
+      steps: ['Fry rice', 'add egg'],
+      persons: 4,
+      time: 15,
+      categories: ['Chinese', 'Rice'],
+      chefId: kaylee.id,
+    },
+    {
+      title: 'Pancakes with chocolate',
+      description: 'This is a delicious pancakes with chocolate',
+      ingredients: ['Pancakes', 'chocolate'],
+      steps: ['Fry pancakes', 'add chocolate'],
+      persons: 6,
+      time: 20,
+      categories: ['Dutch', 'Pancakes'],
+      chefId: tilai.id,
+    },
+    {
+      title: 'Pizza with ham',
+      description: 'This is a delicious pizza with ham',
+      ingredients: ['Pizza', 'ham'],
+      steps: ['Bake pizza', 'add ham'],
+      persons: 2,
+      time: 30,
+      categories: ['Italian', 'Pizza'],
+      chefId: levy.id,
+    },
+  ];
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  for (const recipe of recipes) {
+    await prisma.recipe.create({ data: recipe });
+  }
 
-  const user = await prisma.user.create({
+  console.log(`Database has been seeded. 🌱`);
+}
+
+async function createUser(name: string) {
+  return prisma.user.create({
     data: {
-      email,
+      name: capitalize(name),
+      email: `${name}@dkh.nl`,
+      bio: `Hello, I am ${name}`,
       password: {
         create: {
-          hash: hashedPassword,
+          hash: await bcrypt.hash(name, 10),
         },
       },
     },
   });
+}
 
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  console.log(`Database has been seeded. 🌱`);
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 seed()
